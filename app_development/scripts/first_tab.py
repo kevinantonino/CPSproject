23e54b1e655d413830d4eaa8b6b2b76721541a2f
cwrighta7xx <cwrighta7xx@users.noreply.github.com>
2020-04-20 22:03:16 -0700

    Update first_tab.py

--- app_development/scripts/first_tab.py
+++ app_development/scripts/first_tab.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from datetime import date
+from datetime import datetime
 
 # from bokeh.io import output_file
 from bokeh.layouts import column, row
@@ -11,10 +13,8 @@ from bokeh.io import show
 from bokeh.plotting import figure
 
 from bokeh.models import CategoricalColorMapper, HoverTool, ColumnDataSource, Panel
-from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs, TableColumn, DataTable, RadioGroup, Dropdown
-from bokeh.models.widgets import (CheckboxGroup, Slider, RangeSlider,
-								  Tabs, CheckboxButtonGroup,
-								  TableColumn, DataTable, Select)
+from bokeh.models.widgets import CheckboxGroup, Slider, RangeSlider, Tabs, TableColumn, DataTable, RadioGroup,RadioButtonGroup, Dropdown,DateRangeSlider
+
 
 from bokeh.layouts import column, row, WidgetBox
 from bokeh.palettes import Category20_16
@@ -27,9 +27,13 @@ def first_tab_create(filterData):
     ########method1: create data source for plots
 
     # dummy data that will be replaced by button values once we get those implemented (right now only granulaity button is implemented)
+
+    all_min_date = filterData.groupby('dataid').agg(min)["time"]
+    all_max_date = filterData.groupby('dataid').agg(max)["time"]
+
     dummy_daterange = ['2019-05-01', '2019-08-20']
     dummy_home_id = 27
-    dummy_data_type = 'grid'
+    dummy_data_type = 'car1'
     dummy_granularity = '15 Minutes'
 
     def plot1_data(house, daterange=dummy_daterange, data=dummy_data_type, xaxis=dummy_granularity):
@@ -95,7 +99,7 @@ def first_tab_create(filterData):
         # create the plot every time  a change occurs
         plot1 = figure(title="PLOT 1", x_axis_type="datetime", x_axis_label="Local Time",
                        y_axis_label="Consumption [kW]")
-        plot1.line('time', 'grid', source=src, )  # simple line plot
+        plot1.line('time', "grid", source=src, )  # simple line plot   #data_type_available[data_type_selector.active]
 
         return plot1  # plot object type
 
@@ -108,21 +112,51 @@ def first_tab_create(filterData):
 
     def update(attr, old, new):  # still a little unsure how the update function gets values passed in implicitly
         # these values to be replaced with button/user inputs
-        home_id_to_plot = 27
-        daterange_to_plot = ['2019-05-01', '2019-08-20']
-        data_type_to_plot = 'grid'
+
+        home_id_to_plot = int(home_id_selector.value)
+        print(attr, old, new)
+
+        #         if home_id_selector.value == new:
+
+        #             daterange_to_plot = [all_min_date[home_id_to_plot].strftime("%Y-%m-%d"), all_max_date[home_id_to_plot].strftime("%Y-%m-%d")]
+        #             date_range_slider.start=all_min_date[home_id_to_plot]
+        #             date_range_slider.end=all_max_date[home_id_to_plot]
+        #             date_range_slider.value=(all_min_date[home_id_to_plot],all_max_date[home_id_to_plot])
+
+        #             print("if 1 truth")
+        #         else:
+        daterange_raw = list(date_range_slider.value_as_datetime)
+        daterange_to_plot = [daterange_raw[0].strftime("%Y-%m-%d"), daterange_raw[1].strftime("%Y-%m-%d")]
+        #         print("falsyy")
+
+        date_range_slider.update(start=all_min_date[home_id_to_plot], end=all_max_date[home_id_to_plot],
+                                 value=(all_min_date[home_id_to_plot], all_max_date[home_id_to_plot]))
+
+        #         print("after:",new)
+
+        #         print(attr,old,new)
+
+        #         daterange_to_plot=list(string_date1,string_date2)
+
+        #         daterange_to_plot=dummy_daterange
+
+        data_type_to_plot = "grid"  # data_type_available[data_type_selector.active]
 
         # only working button call so far
         granularity_to_plot = granularity_1.labels[granularity_1.active]
 
         #         granularity_to_plot= [granularity_1.labels[i] for i in granularity_1.active]
-
+        print("before data create")
         # create a new data source
         new_src1 = plot1_data(home_id_to_plot, daterange=daterange_to_plot, data=data_type_to_plot,
                               xaxis=granularity_to_plot)
-
+        print("before data update")
         # push new data  to the source data the rest of the app is usig for plot1
         src1.data.update(new_src1.data)
+        print("updated data")
+
+    #     def new_home(attr,old,new):
+    #         return
 
     ############# Add widgets
 
@@ -134,23 +168,32 @@ def first_tab_create(filterData):
                             update)  # not sure exactly how this works but runs update on the change of the button and passes through the value of the button
 
     home_ids_available = np.unique(filterData['dataid'])
-    home_ids_available= list(map(str, home_ids_available))
-    home_id_selector = Dropdown(label="Home ID to Plot", button_type="warning", menu=home_ids_available)
+    home_ids_available = list(map(str, home_ids_available))
+    home_id_selector = Dropdown(label="Home ID to Plot", button_type="warning", menu=home_ids_available, value="27")
 
     home_id_selector.on_change('value', update)
 
+    date_range_slider = DateRangeSlider(title="Date Range: ", start=date(2019, 5, 1), end=date(2019, 8, 20),
+                                        value=(date(2019, 5, 1), date(2019, 8, 20)), step=1)
+    date_range_slider.on_change("value", update)
+
+    # data_type_selector = RadioButtonGroup(labels=["Net Home Consumption","Solar Generation","EV Consumption"],active=0)
+
+    # data_type_selector.on_change('active',update)
+    # data_type_available=["grid","solar","car1"]
+
     ############ Initialize opening plot and data
-    src1 = plot1_data(27, ['2019-05-01', '2019-08-20'], 'grid', 'Day')  # start with a data range we know is correct
+    src1 = plot1_data(int(home_ids_available[0]), ['2019-05-01', '2019-08-20'], 'grid',
+                      '15 Minutes')  # start with a data range we know is correct
     plot1 = plot1_plot(src1)
 
     ##### Formatting of the app screen
 
     # Put controls in a single element (add more later to format)
-    controls = WidgetBox(granularity_1,home_id_selector)
+    controls = WidgetBox(granularity_1, home_id_selector, date_range_slider)  # data_type_selector)
 
     # Create a row layout
     layout = row(controls, plot1)
-
     # Make a tab with the layout
     tab = Panel(child=layout, title='First Tab')
 

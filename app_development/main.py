19458d1b6dcfee7807fd9ea1081705723f2c6d87
cwrighta7xx <cwrighta7xx@users.noreply.github.com>
2020-07-11 10:15:17 -0700

    Update main.py

--- app_development/main.py
+++ app_development/main.py
@@ -18,7 +18,7 @@ from scripts.second_tab import second_tab_create
 
 # Read data into dataframes
 allData = pd.read_csv(join(dirname(__file__), 'data', '15min_EV_PV_homes_only.csv'))
-enel_data= pd.read_csv(join(dirname(__file__), 'data', 'cleaned_enel_data3.csv'))
+enel_data= pd.read_csv(join(dirname(__file__), 'data', 'cleaned_enel_data_with_nans.csv'))
 
 
 
@@ -28,7 +28,7 @@ filterData = filterData.rename(columns={"local_15min":"time"})
 filterData["time"] = pd.to_datetime(filterData["time"]) # change to appropriate data type
 
 enel_data["time"]= pd.to_datetime(enel_data["time"]) # change to appropriate data type
-enel_data["grid"]= enel_data["solar"]-enel_data["load"] #create grid value
+# enel_data["grid"]= enel_data["solar"]-enel_data["load"] #create grid value
 
 # Create each of the tabs
 tab1 = first_tab_create(filterData,enel_data)

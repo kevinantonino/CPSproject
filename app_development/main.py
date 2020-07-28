fe1db4dbe0d94e9aa8f4a92d9f8097c16fc2917e
kevinantonino <62574499+kevinantonino@users.noreply.github.com>
2020-07-28 16:47:20 -0700

    Add files via upload

--- app_development/main.py
+++ app_development/main.py
@@ -1,6 +1,5 @@
 # Pandas for data management
 import pandas as pd
-import numpy as np
 
 
 # os methods for manipulating paths
@@ -14,22 +13,60 @@ from bokeh.models.widgets import Tabs
 # Each tab is drawn by one script
 from scripts.first_tab import first_tab_create
 from scripts.second_tab import second_tab_create
+from scripts.third_tab import third_tab_create
 
 
 # Read data into dataframes
 allData = pd.read_csv(join(dirname(__file__), 'data', '15min_EV_PV_homes_only.csv'))
-
 filterData = allData[["car1","grid","solar","local_15min","dataid","state"]] # cutting down nonessential columns for the sake of runtime
 filterData['load'] = filterData['grid'] + filterData['solar']
 filterData = filterData.rename(columns={"local_15min":"time"})
-filterData["time"] = pd.to_datetime(filterData["time"]) # change to appropriate data type
+
+
+## NY Removing Tz info
+a = pd.to_datetime(filterData[filterData['state'] == 'NY']['time'], utc = True)  - pd.DateOffset(hours=5)
+b = filterData[filterData['state'] == 'NY']
+b['time'] = a.loc[:]
+filterData = filterData[filterData['state'] == 'TX']
+filterData = filterData.append(b)
+
+
+## NY Aggregate house
+agg = filterData.groupby(filterData[filterData['state']=='NY']['time']).sum()
+agg['dataid'] = 1
+agg['time'] = agg.index
+agg['time'] = pd.to_datetime(agg['time'])
+agg['state'] = 'NY'
+agg = agg[['car1','grid','solar','time','dataid','state','load']]
+filterData = filterData.append(agg)
+
+
+## TX removing Tz info
+a = pd.to_datetime(filterData[filterData['state'] == 'TX']['time'], utc = True)  - pd.DateOffset(hours=6)
+b = filterData[filterData['state'] == 'TX']
+b['time'] = a.loc[:]
+filterData = filterData[filterData['state'] == 'NY']
+filterData = filterData.append(b)
+
+
+## Enel
+enelData = pd.read_csv(join(dirname(__file__), 'data', 'cleaned_enel_data_with_nans.csv'))
+enelData = enelData.drop(columns = 'local_15min')
+enelData['time'] = pd.to_datetime(enelData['time'], utc = True)
+enelData['state'] = 'Italy'
+enelData = enelData[['car1','grid','solar','time','dataid','state','load']]
+filterData = filterData.append(enelData)
+
+
+filterData['time'] = pd.to_datetime(filterData['time'], utc = True)
 
 # Create each of the tabs
 tab1 = first_tab_create(filterData)
 tab2 = second_tab_create(filterData)
+tab3 = third_tab_create(filterData)
 
 # Put all the tabs into one application
-tabs = Tabs(tabs = [tab1,tab2])
+tabs = Tabs(tabs = [tab1,tab2,tab3])
 
 # Put the tabs in the current document for display
 curdoc().add_root(tabs)

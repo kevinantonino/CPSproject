8224f87c67e659b52baf19707fb4cebeb0885f98
cwrighta7xx <cwrighta7xx@users.noreply.github.com>
2021-02-07 22:23:34 -0500

    Update main.py

--- app_development/main.py
+++ app_development/main.py
@@ -1,6 +1,6 @@
 # Pandas for data management
 import pandas as pd
-
+import numpy as np
 
 # os methods for manipulating paths
 from os.path import dirname, join
@@ -21,6 +21,7 @@ allData = pd.read_csv(join(dirname(__file__), 'data', '15min_EV_PV_homes_only.cs
 filterData = allData[["car1","grid","solar","local_15min","dataid","state"]] # cutting down nonessential columns for the sake of runtime
 filterData['load'] = filterData['grid'] + filterData['solar']
 filterData = filterData.rename(columns={"local_15min":"time"})
+filterData["country"]= np.full((len(filterData),1), "USA")
 
 
 ## NY Removing Tz info
@@ -37,35 +38,35 @@ b['time'].loc[:] = a.loc[:]
 filterData = filterData[filterData['state'] == 'NY']
 filterData = filterData.append(b)
 
-
-## NY Aggregate house
-agg = filterData.groupby(filterData[filterData['state']=='NY']['time']).sum()
-agg['dataid'] = 1
-agg['time'] = agg.index
-agg['time'] = pd.to_datetime(agg['time'])
-agg['state'] = 'NY'
-agg = agg[['car1','grid','solar','time','dataid','state','load']]
-filterData = filterData.append(agg)
-
-## TX Aggregate house
-agg = filterData.groupby(filterData[filterData['state']=='TX']['time']).sum()
-agg['dataid'] = 2
-agg['time'] = agg.index
-agg['time'] = pd.to_datetime(agg['time'])
-agg['state'] = 'TX'
-agg = agg[['car1','grid','solar','time','dataid','state','load']]
-filterData = filterData.append(agg)
-
-## TX Aggregate house
-agg = filterData.groupby(filterData['time']).sum()
-agg['dataid'] = 3
-agg['time'] = agg.index
-agg['time'] = pd.to_datetime(agg['time'])
-agg['state'] = 'TXNY'
-agg = agg[['car1','grid','solar','time','dataid','state','load']]
-filterData = filterData.append(agg)
-
-#took out original enel data because solar readings do not make sense
+#
+# ## NY Aggregate house
+# agg = filterData.groupby(filterData[filterData['state']=='NY']['time']).sum()
+# agg['dataid'] = 1
+# agg['time'] = agg.index
+# agg['time'] = pd.to_datetime(agg['time'])
+# agg['state'] = 'NY'
+# agg = agg[['car1','grid','solar','time','dataid','state','load']]
+# filterData = filterData.append(agg)
+#
+# ## TX Aggregate house
+# agg = filterData.groupby(filterData[filterData['state']=='TX']['time']).sum()
+# agg['dataid'] = 2
+# agg['time'] = agg.index
+# agg['time'] = pd.to_datetime(agg['time'])
+# agg['state'] = 'TX'
+# agg = agg[['car1','grid','solar','time','dataid','state','load']]
+# filterData = filterData.append(agg)
+#
+# ## TX Aggregate house
+# agg = filterData.groupby(filterData['time']).sum()
+# agg['dataid'] = 3
+# agg['time'] = agg.index
+# agg['time'] = pd.to_datetime(agg['time'])
+# agg['state'] = 'TXNY'
+# agg = agg[['car1','grid','solar','time','dataid','state','load']]
+# filterData = filterData.append(agg)
+#
+# #took out original enel data because solar readings do not make sense
 # ## Enel
 # enelData = pd.read_csv(join(dirname(__file__), 'data', 'cleaned_enel_data_with_nans.csv'))
 # enelData = enelData.drop(columns = 'local_15min')
@@ -91,6 +92,8 @@ newEnel["car1"] = newEnel["car1"]/1000
 newEnel["solar"] = newEnel["solar"]/1000
 newEnel["load"] = newEnel["load"]/1000
 newEnel["grid"] = newEnel["grid"]/1000
+newEnel["country"] = np.full((len(newEnel),1), "Italy")
+
 
 
 
@@ -104,10 +107,11 @@ filterData['time'] = pd.to_datetime(filterData['time'], utc = True)
 # Create each of the tabs
 tab1 = first_tab_create(filterData)
 tab2 = second_tab_create(filterData)
-tab3 = third_tab_create(filterData)
+#tab3 = third_tab_create(filterData)
 
 
-tabs = Tabs(tabs = [tab1,tab2,tab3])
+tabs = Tabs(tabs = [tab1,tab2])
+#tabs = Tabs(tabs = [tab1,tab2,tab3])
 
 
 # Put the tabs in the current document for display

1ed995f3ab2c45faff03aa1f9270a7131a98244c
cwrighta7xx <cwrighta7xx@users.noreply.github.com>
2021-02-11 18:00:05 -0500

    Update main.py

--- app_development/main.py
+++ app_development/main.py
@@ -15,7 +15,6 @@ from scripts.first_tab import first_tab_create
 from scripts.second_tab import second_tab_create
 from scripts.third_tab import third_tab_create
 
-
 # Read data into dataframes
 allData = pd.read_csv(join(dirname(__file__), 'data', '15min_EV_PV_homes_only.csv'))
 filterData = allData[["car1","grid","solar","local_15min","dataid","state"]] # cutting down nonessential columns for the sake of runtime
@@ -38,34 +37,6 @@ b['time'].loc[:] = a.loc[:]
 filterData = filterData[filterData['state'] == 'NY']
 filterData = filterData.append(b)
 
-#
-# ## NY Aggregate house
-# agg = filterData.groupby(filterData[filterData['state']=='NY']['time']).sum()
-# agg['dataid'] = 1
-# agg['time'] = agg.index
-# agg['time'] = pd.to_datetime(agg['time'])
-# agg['state'] = 'NY'
-# agg = agg[['car1','grid','solar','time','dataid','state','load']]
-# filterData = filterData.append(agg)
-#
-# ## TX Aggregate house
-# agg = filterData.groupby(filterData[filterData['state']=='TX']['time']).sum()
-# agg['dataid'] = 2
-# agg['time'] = agg.index
-# agg['time'] = pd.to_datetime(agg['time'])
-# agg['state'] = 'TX'
-# agg = agg[['car1','grid','solar','time','dataid','state','load']]
-# filterData = filterData.append(agg)
-#
-# ## TX Aggregate house
-# agg = filterData.groupby(filterData['time']).sum()
-# agg['dataid'] = 3
-# agg['time'] = agg.index
-# agg['time'] = pd.to_datetime(agg['time'])
-# agg['state'] = 'TXNY'
-# agg = agg[['car1','grid','solar','time','dataid','state','load']]
-# filterData = filterData.append(agg)
-#
 # #took out original enel data because solar readings do not make sense
 # ## Enel
 # enelData = pd.read_csv(join(dirname(__file__), 'data', 'cleaned_enel_data_with_nans.csv'))
@@ -77,7 +48,6 @@ filterData = filterData.append(b)
 
 
 # new Enel with 15 min resample (79 homes, id 500 - 609)
-
 newEnel = pd.read_csv(join(dirname(__file__), 'data', 'new_Enel_15min_resamp.csv'))
 #newEnel.index() = newEnel['time']
 #newEnel = newEnel.drop(columns = 'time')
@@ -94,22 +64,17 @@ newEnel["load"] = newEnel["load"]/1000
 newEnel["grid"] = newEnel["grid"]/1000
 newEnel["country"] = np.full((len(newEnel),1), "Italy") #add country column
 
-
 ###not sure about time of enel data, make sure it is in local time
 filterData = filterData.append(newEnel)
 filterData['time'] = pd.to_datetime(filterData['time'], utc = True)
 
-
-
 # Create each of the tabs
 tab1 = first_tab_create(filterData)
 tab2 = second_tab_create(filterData)
-#tab3 = third_tab_create(filterData)
+tab3 = third_tab_create(filterData)
 
-
-tabs = Tabs(tabs = [tab1,tab2])
-#tabs = Tabs(tabs = [tab1,tab2,tab3])
-
+# tabs = Tabs(tabs = [tab1,tab2])
+tabs = Tabs(tabs = [tab1,tab2,tab3])
 
 # Put the tabs in the current document for display
 curdoc().add_root(tabs)
